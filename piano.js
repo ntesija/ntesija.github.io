acaa6fef0100ac8d1c9aa3f96ebc8d2ab03ab377
Nik Tesija <ntesija@gmail.com>
2025-01-06 10:56:10 +0000

    Added all my new pages, changed my icon

---
+++ piano.js
@@ -0,0 +1,295 @@
+const PRESSED_CLASS = 'pressed';
+const MAX_OSCILLATORS = 3;
+const MAIN_GAIN = 1;
+const GAIN_OFF = 0;
+const GAIN_ON = (1 / MAX_OSCILLATORS) ?? 1;
+const DEFAULT_WAVE = 'sawtooth';
+const MAX_VOICES = 6;
+const WAVEFORMS = [
+   'sawtooth',
+   'sine',
+   'square',
+   'triangle',
+];
+
+const keys = document.querySelectorAll('.key');
+const optionsEl = document.getElementById('options');
+const oscillatorOptions = document.getElementById('oscillator-options');
+const voices = [];
+
+const freqToKeyMap = new Map([
+   [261.6256, 'KeyA'],
+   [277.1826, 'KeyW'],
+   [293.6648, 'KeyS'],
+   [311.127, 'KeyE'],
+   [329.6276, 'KeyD'],
+   [349.2282, 'KeyF'],
+   [369.9944, 'KeyT'],
+   [391.9954, 'KeyG'],
+   [415.3047, 'KeyY'],
+   [440, 'KeyH'],
+   [466.1638, 'KeyU'],
+   [493.8833, 'KeyJ'],
+   [523.2511, 'KeyK']
+]);
+
+let currentVoiceCount = 4;
+let audioCtx;
+let masterGain;
+
+class Voice {
+   isActive = false;
+   activeTime = -1;
+   frequency = -1;
+   oscillators = [];
+   oscillatorGains = [];
+
+   constructor(audioCtx, voiceIndicatorEl) {
+      this.audioCtx = audioCtx;
+      this.voiceIndicatorEl = voiceIndicatorEl;
+
+      this.voiceGain = new GainNode(audioCtx, { gain: GAIN_OFF })
+      this.voiceGain.connect(masterGain);
+
+      for (let i = 0; i < MAX_OSCILLATORS; i++) {
+         const oscillatorGain = new GainNode(audioCtx, { gain: GAIN_ON });
+         this.oscillatorGains.push(oscillatorGain);
+
+         const oscillator = new OscillatorNode(audioCtx, { frequency: 0, type: DEFAULT_WAVE });
+         oscillator.connect(oscillatorGain);
+         oscillator.start();
+         this.oscillators.push(oscillator);
+         oscillatorGain.connect(this.voiceGain);
+      }
+   }
+
+   playFrequency(frequency) {
+      if (this.activeTime > -1) {
+         const keyElement = document.querySelector(`[data-freq="${this.frequency}"]`);
+         keyElement?.classList.remove(PRESSED_CLASS);
+      }
+
+      const currentTime = this.audioCtx.currentTime;
+      for (const oscillator of this.oscillators) {
+         oscillator.frequency.setValueAtTime(frequency, currentTime);
+      }
+
+      this.voiceGain.gain.setValueAtTime(GAIN_ON, currentTime);
+      this.isActive = true;
+      this.activeTime = currentTime;
+      this.frequency = frequency;
+
+      this.voiceIndicatorEl.classList.add('on');
+   }
+
+   stop() {
+      this.voiceGain.gain.setValueAtTime(GAIN_OFF, this.audioCtx.currentTime);
+      this.isActive = false;
+      this.frequency = -1;
+
+      this.voiceIndicatorEl.classList.remove('on');
+   }
+
+   setOscillatorType(newWave, oscillator) {
+      this.oscillators[oscillator].type = newWave;
+
+   }
+
+   setOscillatorGain(gainValue, oscillator) {
+      this.oscillatorGains[oscillator].gain.setValueAtTime(gainValue, this.audioCtx.currentTime);
+   }
+}
+
+function getVoiceIndicatorElements() {
+   const voiceIndicators = document.querySelectorAll('[data-voice-indicator]');
+   if (voiceIndicators.length !== MAX_VOICES) {
+      console.error(`expected ${MAX_VOICES} indicator elements but got ${voiceIndicators.length}`);
+   }
+
+   return voiceIndicators;
+}
+
+function instantiateVoices() {
+   if (!audioCtx) {
+      console.warn('attempting to instantiate new voices but audioCtx is not set', `audioCtx: ${audioCtx}`);
+      return;
+   }
+
+   const voiceIndicators = getVoiceIndicatorElements();
+   for (let i = 0; i < MAX_VOICES; i++) {
+      const voiceIndicatorEl = voiceIndicators[i];
+      voices.push(new Voice(audioCtx, voiceIndicatorEl));
+   }
+}
+
+function setVoiceIndicators(numIndicators) {
+   const voiceIndicators = getVoiceIndicatorElements();
+   for (let i = 0; i < voiceIndicators.length; i++) {
+      if (i < numIndicators) {
+         voiceIndicators[i].classList.remove('disabled');
+      } else {
+         voiceIndicators[i].classList.add('disabled');
+      }
+   }
+}
+
+function createVoiceSelector() {
+   const voiceSelector = document.querySelector('[data-voices]');
+   voiceSelector.type = 'number';
+   voiceSelector.value = currentVoiceCount;
+   voiceSelector.max = MAX_VOICES;
+   voiceSelector.min = 1;
+
+   const voiceTemplate = document.getElementById('voice-indicator');
+   const voiceContainer = document.getElementById('voices');
+
+   for (let i = 0; i < MAX_VOICES; i++) {
+      const voiceEl = voiceTemplate.content.cloneNode(true);
+      voiceContainer.appendChild(voiceEl);
+   }
+
+   setVoiceIndicators(currentVoiceCount);
+
+   voiceSelector.addEventListener('change', event => {
+      currentVoiceCount = event.target.value;
+      setVoiceIndicators(currentVoiceCount);
+   });
+}
+
+
+function createOscillatorOption(oscillatorNum) {
+   const template = document.getElementById('oscillator');
+   const oscillatorContainer = template.content.cloneNode(true);
+
+   const titleEl = oscillatorContainer.querySelector('[data-oscillator-title');
+   titleEl.textContent = `Oscillator ${oscillatorNum + 1}`
+
+   const labelEl = oscillatorContainer.querySelector('[data-wave-label]');
+   labelEl.textContent = `Waveform`;
+   const selectEl = oscillatorContainer.querySelector('[data-wave-select]');
+   selectEl.value = DEFAULT_WAVE;
+
+   for (const wave of WAVEFORMS) {
+      const option = document.createElement('option');
+      option.label = wave;
+      option.value = wave;
+      selectEl.add(option);
+   }
+
+   selectEl.addEventListener('change', event => {
+      if (!WAVEFORMS.includes(event.target.value)) {
+         console.warn(`failed to set waveform to ${event.target.value}`);
+         return;
+      }
+
+      for (const voice of voices) {
+         voice.setOscillatorType(event.target.value, oscillatorNum);
+      }
+   });
+
+   const gainEl = oscillatorContainer.querySelector('[data-gain]');
+   const gainInputScale = 100;
+   gainEl.max = gainInputScale;
+   gainEl.value = gainInputScale;
+   gainEl.addEventListener('change', event => {
+      const normalizedVolume = event.target.value / (MAX_OSCILLATORS * gainInputScale);
+      for (const voice of voices) {
+         voice.setOscillatorGain(normalizedVolume, oscillatorNum);
+      }
+   });
+
+   oscillatorOptions.appendChild(oscillatorContainer);
+}
+
+function drawAnalayzer(audioContext, source) {
+   const analyser = audioContext.createAnalyser();
+   analyser.fftSize = 2048;
+
+   const bufferLength = analyser.frequencyBinCount;
+   const dataArray = new Uint8Array(bufferLength);
+   analyser.getByteTimeDomainData(dataArray);
+
+   // Connect the source to be analyzed
+   source.connect(analyser);
+
+   // Get a canvas defined with ID "oscilloscope"
+   const canvas = document.getElementById("oscilloscope");
+   const canvasCtx = canvas.getContext("2d");
+
+   function draw() {
+      requestAnimationFrame(draw);
+
+      analyser.getByteTimeDomainData(dataArray);
+
+      canvasCtx.fillStyle = '#a7ffbe';
+      canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+
+      canvasCtx.lineWidth = 2;
+      canvasCtx.strokeStyle = "rgb(0 0 0)";
+
+      canvasCtx.beginPath();
+
+      const sliceWidth = (canvas.width * 1.0) / bufferLength;
+      let x = 0;
+
+      for (let i = 0; i < bufferLength; i++) {
+         const v = dataArray[i] / 128.0;
+         const y = (v * canvas.height) / 2;
+
+         if (i === 0) {
+            canvasCtx.moveTo(x, y);
+         } else {
+            canvasCtx.lineTo(x, y);
+         }
+
+         x += sliceWidth;
+      }
+
+      canvasCtx.lineTo(canvas.width, canvas.height / 2);
+      canvasCtx.stroke();
+   }
+
+   draw();
+}
+
+addEventListener('mousedown', () => {
+   audioCtx = new AudioContext();
+   masterGain = new GainNode(audioCtx, { gain: MAIN_GAIN });
+   masterGain.connect(audioCtx.destination);
+   drawAnalayzer(audioCtx, masterGain);
+
+   instantiateVoices();
+
+   for (const key of keys) {
+      const frequency = Number(key.getAttribute('data-freq')) ?? 0;
+      const keyCode = freqToKeyMap.get(frequency) ?? '';
+
+      addEventListener('keydown', event => {
+         if (event.code !== keyCode || event.repeat) return;
+
+         const currentVoiceSet = voices.slice(0, currentVoiceCount);
+         const inactiveVoces = currentVoiceSet.filter(voice => !voice.isActive);
+         // Use an inactive voice before we reuse active ones
+         const voicePool = inactiveVoces.length > 0 ? inactiveVoces : currentVoiceSet;
+         const nextVoice = voicePool.toSorted((a, b) => a.activeTime - b.activeTime)[0];
+         nextVoice.playFrequency(frequency);
+         key.classList.add(PRESSED_CLASS);
+      });
+
+      addEventListener('keyup', event => {
+         if (event.code !== keyCode) return;
+
+         const activeVoice = voices.find(voice => voice.frequency === frequency);
+         if (!activeVoice) return;
+
+         activeVoice.stop();
+         key.classList.remove(PRESSED_CLASS);
+      });
+   }
+}, { once: true });
+
+createVoiceSelector();
+
+for (let i = 0; i < MAX_OSCILLATORS; i++) {
+   createOscillatorOption(i);
+}
---
+++ site-footer.js
@@ -0,0 +1,73 @@
+const linkMap = new Map([
+   ['home', 'index.html'],
+   ['about', 'about.html'],
+   ['piano', 'piano.html'],
+])
+
+class SiteFooter extends HTMLElement {
+   constructor() {
+      super();
+   }
+
+   getCurrentLocation() {
+      return document.location.href.split('/')?.reverse()?.[0] ?? '';
+   }
+
+   createLinkTable() {
+      const linkTable = document.createElement('table');
+      const linkTableRow = document.createElement('tr');
+      linkTable.appendChild(linkTableRow);
+      for (const [title, link] of linkMap.entries()) {
+         const linkTableCell = document.createElement('td');
+         const linkTemplate = document.createElement('a');
+         linkTemplate.href = link;
+         linkTemplate.textContent = title;
+
+         if (link === this.getCurrentLocation()) {
+            linkTableCell.classList.add('active')
+         }
+
+         linkTableCell.appendChild(linkTemplate);
+         linkTableRow.appendChild(linkTableCell);
+      }
+
+      return linkTable;
+   }
+
+   createStyle() {
+      const style = document.createElement('style');
+      style.textContent = `
+         table, tr, td {
+            border: 1px solid black;
+         }
+
+         td.active {
+            background: #ffffffc0;
+            border-style: dashed;
+         }
+
+         td.active a {
+            color: black;
+         }
+            
+         footer {
+            font-size: 24px;
+            position: fixed;
+            bottom: 8px;
+            left: 8px;
+         }`;
+      return style;
+   }
+
+   connectedCallback() {
+      const shadow = this.attachShadow({ mode: "open" });
+      const template = document.createElement('footer');
+
+      template.appendChild(this.createStyle());
+      template.appendChild(this.createLinkTable());
+
+      shadow.appendChild(template.cloneNode(true));
+   }
+}
+
+customElements.define('site-footer', SiteFooter);
